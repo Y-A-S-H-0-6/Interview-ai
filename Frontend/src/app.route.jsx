e1c2a50a1fb48850909f9c0c214e0fe5d315c557
Yash Patidar <yashpatidar862@gmail.com>
2026-04-23 22:07:45 +0530

    Update app.route.jsx

--- Frontend/src/app.route.jsx
+++ Frontend/src/app.route.jsx
@@ -3,7 +3,7 @@ import Login from "./features/auth/pages/login.jsx"
 import Register from "./features/auth/pages/register.jsx"
 import Protected from "./features/auth/components/Protected.jsx"
 import Home from "./features/interview/pages/Home.jsx"
-import Interview from "./features/interview/pages/interview.jsx"
+import Interview from "./features/interview/pages/Interview.jsx"
 
 
 export const router = createBrowserRouter([
