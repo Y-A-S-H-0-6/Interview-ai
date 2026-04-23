a8018e0f3798a1a344d301766cc84cbe838263e6
Yash Patidar <yashpatidar862@gmail.com>
2026-04-23 22:20:34 +0530

    Update app.js

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -9,7 +9,10 @@ const app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin: "http://localhost:5173",
+ origin: [
+        "http://localhost:5173",
+        "https://interview-ai-frontend-vyb2.onrender.com"
+    ],    
     credentials: true
 }))
 
